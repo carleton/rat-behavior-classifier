5ce3b851e3e7b6f9c452db962ebf5dde830a35bb
Kevin Chen <usa.kevin@gmail.com>
2021-10-12 14:33:25 -0500

    added comments

--- trackConverter.py
+++ trackConverter.py
@@ -6,11 +6,11 @@ import cv2
 import math
 import os
 
-# Returns distance between two coordinates
+# Returns distance between a (x,y) coordinate pair
 def lengthFinder(x1,y1,x2,y2):
     return math.sqrt((x1-x2)**2 + (y1-y2)**2)
 
-# Returns average of given coordinates
+# Returns average of the given coordinates. If none of them have a value, return NaN. 
 def getCenter(positions):
     total, count = 0, 0
     for position in [pos for pos in positions if pos is not None]:
@@ -21,15 +21,18 @@ def getCenter(positions):
 
     return total / count
 
+# returns properly formatted tracks for a given animal
 def getTracks(track_list, offset):
-    x = []
-    y = []
-    a_list = []
-    b_list = []
-
-    # 4 because of headers in CSV
-    theta_list = []
+    x = [] # holds list of x coordinate values
+    y = [] # holds list of y coordinate values
+    a_list = [] # holds list of "a" coordinate values (a = 1/4 of major axis length)
+    b_list = [] # holds list of "b" coordinate values (a = 1/4 of minor axis length)
+    theta_list = [] # holds list of theta values (currently just putting NaN if nothing is found within that frame, 0 otherwise.)
+    
+    # start at 4 to skip headers in CSV
     for row in track_list[4:]:
+        # these rows get the appropriate data from the CSV. "offset" is used to get the appropriate data for the sex of the animal(in this case, 
+        # it's 0 if male, 12 if female)
         nose_x = float(row[1 + offset]) if row[1 + offset] else None
         nose_y = float(row[2 + offset]) if row[2 + offset] else None
         leftear_x = float(row[4 + offset]) if row[4 + offset] else None
@@ -39,53 +42,56 @@ def getTracks(track_list, offset):
         tailbase_x = float(row[10 + offset]) if row[10 + offset] else None
         tailbase_y = float(row[11 + offset]) if row[11 + offset] else None
 
-        x_element = getCenter([nose_x, leftear_x, rightear_x, tailbase_x])
+        # these rows calculate the "center" from the coordinates that are visible in the frame and append it to the appropriate list
+        x_element = getCenter([nose_x, leftear_x, rightear_x, tailbase_x]) 
         y_element = getCenter([nose_y, leftear_y, rightear_y, tailbase_y])
         x.append(float(x_element))
         y.append(float(y_element))
 
-        new_value_flag = math.isnan(x_element) or math.isnan(y_element)
-
-        if nose_x and nose_y and tailbase_x and tailbase_y:
-            a_list.append(float(lengthFinder(nose_x, nose_y, tailbase_x, tailbase_y) / 4)) 
-        else:
-            a_list.append(float("nan"))
-
-        if leftear_x and leftear_y and rightear_x and rightear_y:
-            b_list.append(float(lengthFinder(leftear_x, leftear_y, rightear_x, rightear_y) / 4)) 
-        else:
-            b_list.append(float("nan"))
+        # boolean flag that determines whether we should add 0 or NaN to theta_list. 
+        new_value_flag = math.isnan(x_element) or math.isnan(y_element) 
 
         if new_value_flag:
             theta_list.append(float(0))
         else:
             theta_list.append(float("nan"))
+            
+        # appends to a_list if necessary coordinates are found in the frame
+        if nose_x and nose_y and tailbase_x and tailbase_y:
+            a_list.append(float(lengthFinder(nose_x, nose_y, tailbase_x, tailbase_y) / 4)) 
+        else:
+            a_list.append(float("nan"))
+
+        # appends to b_list if necessary coordinates are found in the frame
+        if leftear_x and leftear_y and rightear_x and rightear_y:
+            b_list.append(float(lengthFinder(leftear_x, leftear_y, rightear_x, rightear_y) / 4)) 
+        else:
+            b_list.append(float("nan"))
 
     return x, y, a_list, b_list, theta_list
 
 def main(videoPath, csv_file_path):
-    cap = cv2.VideoCapture(videoPath)
+    cap = cv2.VideoCapture(videoPath) # used to get fps of video
 
     fps = float(cap.get(cv2.CAP_PROP_FPS))
-    dt = float(1 / fps)
-    print(fps)
-    print(dt)
+    dt = float(1 / fps) # time between frames, which is simply 1 / fps
 
     #read csv
     with open(csv_file_path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         list_csv_reader = list(csv_reader)
 
-        male_x, male_y, male_a, male_b, male_theta = getTracks(list_csv_reader, 0)
-        female_x, female_y, female_a, female_b, female_theta = getTracks(list_csv_reader, 12)
+        male_x, male_y, male_a, male_b, male_theta = getTracks(list_csv_reader, 0) # gets male tracks
+        female_x, female_y, female_a, female_b, female_theta = getTracks(list_csv_reader, 12) # gets female tracks
 
+    # check to make sure that all lists are equal length(which is necessary for JAABA)
     assert len(male_x) == len(male_y) == len(male_a) == len(male_b) == len(female_x) == len(female_y) == len(female_a) == len(female_b)
 
-    nframes = float(len(male_x))
+    nframes = float(len(male_x)) # nframes is the same for both animals
 
-    dt_list = [dt] * int((nframes - 1))
+    dt_list = [dt] * int((nframes - 1)) # time between frames is also the same
 
-    #write mat struct
+    # create the final matlab struct, which is a list of dictionaries(whose fields hold all the tracks)
     trx_array = []
     male_trx_dict = {
         "x" : male_x,
@@ -107,6 +113,7 @@ def main(videoPath, csv_file_path):
         "dt": dt_list, 
         "fps" : fps
     }
+    # this makes sure that the values of all fields(except for sex) are of type float, which is necessary for JAABA
     for val in male_trx_dict.values():
         if val == "m":
             continue
@@ -137,16 +144,15 @@ def main(videoPath, csv_file_path):
     trx_array.append(male_trx_dict)
     trx_array.append(female_trx_dict)
 
-    mat_file = loadmat('template_trx.mat')
+    mat_file = loadmat('template_trx.mat') # loads in template mat file
 
-    transposed = np.transpose([list(male_trx_dict.values()), list(female_trx_dict.values())])
-    mat_file["trx"] = fromarrays(transposed, names = list(male_trx_dict.keys()))
+    transposed = np.transpose([list(male_trx_dict.values()), list(female_trx_dict.values())]) # transpose to orient them correctly within MATLAB
+    mat_file["trx"] = fromarrays(transposed, names = list(male_trx_dict.keys())) # replace the "trx" fields within this file
 
-    savemat(f'{os.path.basename(videoPath).split(".")[0].replace("(", "").replace(")", "").replace(" ", "")}Trx.mat', mat_file)
+    # saves this .mat file locally with a new name
+    savemat(f'{os.path.basename(videoPath).split(".")[0].replace("(", "").replace(")", "").replace(" ", "")}Trx.mat', mat_file) 
 
-
-#36 Optimus Progesterone Dose (1).mp4
-#36 Optimus Progesterone DoseDLC_resnet50_RatLabTest3Aug2shuffle1_44500_el_filtered (1).csv
 
 if __name__ == "__main__":
+    # replace below with the name of your video and name of the CSV tracks outputted by DeepLabCut
     main("36 Optimus Progesterone Dose.mp4", "36 Optimus Progesterone DoseDLC_resnet50_RatLabTest3Aug2shuffle1_44500_el_filtered.csv")
